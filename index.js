dbe6e83a01e9d733eede0d79598c7cc5778ed28c
Ku7ara4a <arxwoodj@gmail.com>
2025-02-11 10:19:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -30,19 +30,24 @@ app.post('/user/create', (req, res) => {
         res.status(200).json('User created successfully')
     }
 })
-app.get('/user/id', (req, res) => {
-    if (users.find(user => user.id == req.body.id)) {
+app.get('/user/id', checkID , (req, res) => {
+
+})
+app.get('/user/all', (req, res) => {
+    res.status(200).json(users)
+})
+
+function checkID(req , res , next){
+    if (users.find(user => user.id === req.body.id)) {
         const search = users.filter(user => {
-            return [user.id == req.body.id][0]
+            return [user.id === req.body.id][0]
         })
         res.status(200).json(search)
     } else {
         res.status(400).json('User not exists')
     }
-})
-app.get('/user/all', (req, res) => {
-    res.status(200).json(users)
-})
+}
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
