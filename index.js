d89686e4cdc682d58db492cf9dacd55929cac887
Denis <arxwoodj@gmail.com>
2025-02-16 17:49:33 +0300

    updated middleware

--- index.js
+++ index.js
@@ -30,23 +30,25 @@ app.post('/user/create', (req, res) => {
         res.status(200).json('User created successfully')
     }
 })
-app.get('/user/id', checkID , (req, res) => {
 
+let search
+
+function checkID(req, res, next){
+    if (users.find(user => user.id === req.body.id)){
+        search = users.find(user => user.id === req.body.id)
+        next()
+    } else {
+        res.status(400).json('User not exists')
+    }
+}
+app.get('/user/id', checkID , (req, res) => {
+    console.log("ok")
+    res.status(200).json(search)
 })
 app.get('/user/all', (req, res) => {
     res.status(200).json(users)
 })
 
-function checkID(req , res , next){
-    if (users.find(user => user.id === req.body.id)) {
-        const search = users.filter(user => {
-            return [user.id === req.body.id][0]
-        })
-        res.status(200).json(search)
-    } else {
-        res.status(400).json('User not exists')
-    }
-}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
